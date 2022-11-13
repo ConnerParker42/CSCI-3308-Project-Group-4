8b4b77b5391a1e7aa9849b4a05e8e1d668adde0c
ConnerParker42 <copa5633@colorado.edu>
2022-11-13 12:20:09 -0700

    Home bug is gone and Message page works

--- Project-Code/init_data/create.sql
+++ Project-Code/init_data/create.sql
@@ -5,9 +5,6 @@ CREATE TABLE users (
     password VARCHAR(60) NOT NULL
 );
 
-INSERT INTO users (username, email, password) VALUES ('test', 'test@us.er', '$2b$10$xrI.RP712FOIU6GhZbeE3OBHhOzgudLOoHFfAAVf48oCiesPBhDdC');
-INSERT INTO users (username, email, password) VALUES ('test2', 'test2@us.er', '$2b$10$dafscx05GP3oN6Nkh2GuNOY51.aPR1EZHzw5IlE6MA4uSl/AAO4qK');
-
 DROP TABLE IF EXISTS messages CASCADE;
 CREATE TABLE messages (
     message_id SERIAL PRIMARY KEY,
@@ -16,16 +13,18 @@ CREATE TABLE messages (
     receiver_username TEXT NOT NULL REFERENCES users (username) ON DELETE CASCADE
 );
 
-INSERT INTO messages (message_text, sender_username, receiver_username) values ('Hello there', 'test', 'test2');
-INSERT INTO messages (message_text, sender_username, receiver_username) values ('Heres the reply', 'test2', 'test');
-
-
 DROP TABLE IF EXISTS contacts;
 CREATE TABLE contacts (
     sender_username VARCHAR(60) REFERENCES users (username) ON DELETE CASCADE,
     recipient_username VARCHAR(60) REFERENCES users (username) ON DELETE CASCADE,
     CONSTRAINT contact_pk PRIMARY KEY (sender_username, recipient_username)
 );
+
+INSERT INTO users (username, email, password) VALUES ('test', 'test@us.er', '$2b$10$xrI.RP712FOIU6GhZbeE3OBHhOzgudLOoHFfAAVf48oCiesPBhDdC');
+INSERT INTO users (username, email, password) VALUES ('test2', 'test2@us.er', '$2b$10$dafscx05GP3oN6Nkh2GuNOY51.aPR1EZHzw5IlE6MA4uSl/AAO4qK');
+
+INSERT INTO messages (message_text, sender_username, receiver_username) values ('Hello there', 'test', 'test2');
+INSERT INTO messages (message_text, sender_username, receiver_username) values ('Heres the reply', 'test2', 'test');
 
 INSERT INTO contacts (sender_username, recipient_username) VALUES ('test', 'test2'),
                                                                   ('test2', 'test');
