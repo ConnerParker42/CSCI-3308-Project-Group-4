95a339fb3f3f6b46eac2c90f2aedd5eb44a5f0a5
LeeLee James <leja9931@colorado.edu>
2022-11-15 18:30:23 -0500

    Setup initial test for home.ejs

---
+++ Project-Code/test/home.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+// const home = require('../views/pages/home.js');
+
+describe('home', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+
+});
