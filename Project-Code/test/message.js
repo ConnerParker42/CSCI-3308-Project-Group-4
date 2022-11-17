384f458560390b55f694594c388344e0429dc8f4
LeeLee James <leja9931@colorado.edu>
2022-11-17 15:09:54 -0700

    Setup initial test for message.ejs page

---
+++ Project-Code/test/message.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const message = require('../views/pages/message.ejs');
+
+describe('message', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+
+});
