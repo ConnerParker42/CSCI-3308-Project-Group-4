e195ed30add74614ceea5e37680ad24b854b5553
LeeLee James <leja9931@colorado.edu>
2022-11-17 15:06:00 -0700

    Setup initial test for login.ejs

---
+++ Project-Code/test/login.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const home = require('../views/pages/home.ejs');
+
+describe('login', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+
+});
