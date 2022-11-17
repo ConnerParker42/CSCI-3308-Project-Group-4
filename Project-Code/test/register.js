163258d5d38d1da4fc855ab6afd36cfc119d72a1
LeeLee James <leja9931@colorado.edu>
2022-11-17 15:12:43 -0700

    Setup initial test for register.ejs page

---
+++ Project-Code/test/register.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const register = require('../views/pages/register.ejs');
+
+describe('register', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+
+});
